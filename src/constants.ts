c6cee626d29e05aaa19eebcd2bf8a756a804fd5c
La Thieu Huan <huan.la@onemount.com>
2022-07-04 01:51:11 +0700

    initial config app constants and types

---
+++ src/constants.ts
@@ -0,0 +1,114 @@
+export enum EScreen {
+  CALCULATOR = "Calculator",
+  MY_SETUPS = "MySetups",
+  MY_WEAPONS = "MyWeapons",
+  MY_ARTIFACTS = "MyArtifacts",
+  MY_CHARACTERS = "MyCharacters",
+}
+
+export const WEAPON_TYPES = [
+  "sword",
+  "claymore",
+  "catalyst",
+  "polearm",
+  "bow",
+] as const;
+
+export const ARTIFACT_TYPES = [
+  "flower",
+  "plume",
+  "sands",
+  "goblet",
+  "circlet",
+] as const;
+
+export const ELEMENT_TYPES = [
+  "pyro",
+  "hydro",
+  "electro",
+  "cryo",
+  "geo",
+  "anemo",
+  "dendro",
+] as const;
+
+export const BASE_STAT_TYPE = ["base_hp", "base_atk", "base_def"] as const;
+
+export const FLAT_STAT_TYPES = ["hp", "atk", "def", "em"] as const;
+
+export const ELEMENTAL_DMG_BONUS_TYPES = [
+  "pyro_",
+  "hydro_",
+  "dendro_",
+  "electro_",
+  "anemo_",
+  "cryo_",
+  "geo_",
+] as const;
+
+export const ATTACK_ELEMENT_DMG_BONUS_TYPES = [
+  "pyro_",
+  "hydro_",
+  "dendro_",
+  "electro_",
+  "anemo_",
+  "cryo_",
+  "geo_",
+  "phys_",
+] as const;
+
+export const RNG_PERCENT_STAT_TYPES = [
+  "hp_",
+  "atk_",
+  "def_",
+  "cRate",
+  "cDmg",
+  "healBn",
+  "er",
+  "pyro_",
+  "hydro_",
+  "dendro_",
+  "electro_",
+  "anemo_",
+  "cryo_",
+  "geo_",
+  "phys_",
+] as const;
+
+export const PERCENT_STAT_TYPES = [
+  "hp_",
+  "atk_",
+  "def_",
+  "cRate",
+  "cDmg",
+  "healBn",
+  "er",
+  "shStr",
+  "pyro_",
+  "hydro_",
+  "dendro_",
+  "electro_",
+  "anemo_",
+  "cryo_",
+  "geo_",
+  "phys_",
+  "naAtkSpd",
+  "caAtkSpd",
+] as const;
+
+export const LEVELS = [
+  "1/20",
+  "20/20",
+  "20/40",
+  "40/40",
+  "40/50",
+  "50/50",
+  "50/60",
+  "60/60",
+  "60/70",
+  "70/70",
+  "70/80",
+  "80/80",
+  "80/90",
+  "90/90",
+];
---
+++ src/types.ts
@@ -0,0 +1,34 @@
+import {
+  ELEMENT_TYPES,
+  BASE_STAT_TYPE,
+  FLAT_STAT_TYPES,
+  ELEMENTAL_DMG_BONUS_TYPES,
+  ATTACK_ELEMENT_DMG_BONUS_TYPES,
+  RNG_PERCENT_STAT_TYPES,
+  PERCENT_STAT_TYPES,
+  WEAPON_TYPES,
+  ARTIFACT_TYPES,
+} from "./constants";
+
+export type Nation = "mondstadt" | "liyue" | "inazuma" | "sumeru";
+
+export type Rarity = 1 | 2 | 3 | 4 | 5;
+
+export type Weapon = typeof WEAPON_TYPES[number];
+
+export type Artifact = typeof ARTIFACT_TYPES[number];
+
+export type Element = typeof ELEMENT_TYPES[number];
+
+export type BaseStat = typeof BASE_STAT_TYPE[number];
+
+export type FlatStat = typeof FLAT_STAT_TYPES[number];
+
+export type ElementalDmgBonus = typeof ELEMENTAL_DMG_BONUS_TYPES[number];
+
+export type AttackElementDmgBonus =
+  typeof ATTACK_ELEMENT_DMG_BONUS_TYPES[number];
+
+export type RngPercentStat = typeof RNG_PERCENT_STAT_TYPES[number];
+
+export type PercentStat = typeof PERCENT_STAT_TYPES[number];
