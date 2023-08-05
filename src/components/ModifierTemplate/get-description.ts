8260cb929faebce037f2462572cc3bbd7c146758
lathieuhuan <lathieuhuan52@gmail.com>
2023-08-05 20:41:52 +0700

    update decoCharacterDescription

--- src/components/ModifierTemplate/get-description.ts
+++ src/components/ModifierTemplate/get-description.ts
@@ -1,20 +1,22 @@
-import clsx from "clsx";
-
 export const decoCharacterDescription = (pot: string | number) => {
   if (typeof pot === "string") {
-    return pot.replace(/\{[a-zA-Z0-9 ]+\}%?/g, (match) => {
-      let content: string | number = "";
-      let suffix = "";
+    return pot.replace(/\{[a-zA-Z0-9% ]+\}#\[[a-zA-Z0-9,]+\]/g, (match) => {
+      let [content, colorCode = ""] = match.split("#");
+      let classNames = "";
 
-      if (match[match.length - 1] === "%") {
-        content = +match.slice(1, -2);
-        suffix = "%";
-      } else {
-        content = match.slice(1, -1);
-      }
-      console.log("content", content);
-
-      return `<span class='${clsx("text-green", !isNaN(+content) && "font-bold")}'>${content + suffix}</span>`;
+      colorCode
+        .slice(1, -1)
+        .split(",")
+        .forEach((config, i) => {
+          const colorCodeToCls: Record<string, string> = {
+            B: "font-bold",
+            Gr: "text-green",
+            G: "text-lightgold",
+            anemo: "text-anemo",
+          };
+          classNames += (i ? " " : "") + colorCodeToCls[config];
+        });
+      return `<span class='${classNames}'>${content.slice(1, -1)}</span>`;
     });
   }
   return "";
